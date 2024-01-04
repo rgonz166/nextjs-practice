f3f1e4c8e04593d214612b7d89d8a6ed90864d3e
Raul Gonzalez <rgonz166@gmail.com>
2024-01-03 21:16:08 -0800

    Change: UsersPage to be dynamic and not static

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -8,11 +8,14 @@ interface User {
 }
 
 const UsersPage = async () => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const res = await fetch('https://jsonplaceholder.typicode.com/users', {
+    cache: 'no-store',
+  });
   const users: User[] = await res.json();
   return (
     <>
       <h1>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
       <ol>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
