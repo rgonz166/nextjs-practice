604ea3cde84df31f6d08bfbc69cd00a407d39376
Raul Gonzalez <rgonz166@gmail.com>
2024-01-03 21:45:31 -0800

    Add winter theme

--- app/components/AddToCart.tsx
+++ app/components/AddToCart.tsx
@@ -2,7 +2,11 @@
 import React from 'react';
 
 const AddToCart = () => {
-  return <button onClick={() => console.log('Click')}>Add to cart</button>;
+  return (
+    <button className="btn btn-primary" onClick={() => console.log('Click')}>
+      Add to cart
+    </button>
+  );
 };
 
 export default AddToCart;
