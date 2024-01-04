d0bf415185167297133d8ac45dcb0c5c40326567
Raul Gonzalez <rgonz166@gmail.com>
2024-01-03 21:28:19 -0800

    Move Product Card components to own folder

--- app/components/ProductCard/ProductCard.tsx
+++ app/components/ProductCard/ProductCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import AddToCart from './AddToCart';
+import AddToCart from '../AddToCart';
 import styles from './ProductCard.module.css';
 
 const ProductCard = () => {
