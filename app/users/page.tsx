915b0a4c67919ead342c0837dcdc0af4984ce677
Raul Gonzalez <rgonz166@gmail.com>
2024-01-03 23:30:28 -0800

    Create table using tailwind and daisyui

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -16,11 +16,22 @@ const UsersPage = async () => {
     <>
       <h1>Users</h1>
       <p>{new Date().toLocaleTimeString()}</p>
-      <ol>
-        {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ol>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 };
