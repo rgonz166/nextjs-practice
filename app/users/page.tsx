f22cc70f51a2a88f8a88a0bcc74e4f2b9ec0999d
Raul Gonzalez <rgonz166@gmail.com>
2024-01-03 20:54:33 -0800

    Add: fake user data fetch

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
 
-const UsersPage = () => {
-  return <div>UsersPage</div>;
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  website: string;
+}
+
+const UsersPage = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const users: User[] = await res.json();
+  return (
+    <>
+      <h1>Users</h1>
+      <ol>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ol>
+    </>
+  );
 };
 
 export default UsersPage;
